55d92b3667e3ab0262fb49b797fdf08e38889b4b
Jarosław Sadziński <jaroslaw.sadzinski@gmail.com>
2022-05-06 00:11:16 +0200

    Reimplementing array adding simple map

--- src/lib.spec.ts
+++ src/lib.spec.ts
@@ -1,5 +1,5 @@
-import { computed, globalOwner } from './index';
-import { obsArray, obsJson, obsSet } from './lib';
+import {computed, globalOwner} from './index';
+import {obsArray, obsJson, obsMap, obsSet} from './lib';
 
 afterAll(() => globalOwner.dispose());
 
@@ -24,15 +24,71 @@ test('obsArray is called only once', () => {
     counter++;
     return list.get(0);
   });
+  expect(first.get()).toBe(1);
   expect(counter).toBe(1);
   list.set(1, 10);
+  expect(first.get()).toBe(1);
   expect(counter).toBe(1);
   list.set(2, 10);
+  expect(first.get()).toBe(1);
   expect(counter).toBe(1);
   list.push(5);
+  expect(first.get()).toBe(1);
   expect(counter).toBe(1);
   list.set(0, 5);
+  expect(first.get()).toBe(5);
   expect(counter).toBe(2);
+  expect(list.get()).toStrictEqual([5, 10, 10, 5]);
+
+  list.splice(0, 3, 4);
+  expect(first.get()).toBe(4);
+  expect(list.get(2)).toBe(undefined);
+  expect(list.length()).toBe(2);
+});
+
+test('obsArray map works', () => {
+  const list = obsArray(null, [1, 2, 3]);
+  const double = list.map(x => x * 2);
+  let counter = 0;
+  const zero = computed(null, () => {
+    counter++;
+    return double.get(0);
+  });
+  expect(double.get()).toStrictEqual([2, 4, 6]);
+  expect(zero.get()).toBe(2);
+  expect(counter).toBe(1);
+  list.set(1, 4);
+  expect(counter).toBe(1);
+  expect(zero.get()).toBe(2);
+  expect(double.get()).toStrictEqual([2, 8, 6]);
+});
+
+
+test('obsMap map works', () => {
+  const map = obsMap(null, {});
+  let counter = 0;
+  const zero = computed(null, () => {
+    counter++;
+    return map.has('zero');
+  });
+  map.set('zero1', 1);
+  map.set('zero2', 1);
+  expect(zero.get()).toBe(false);
+  expect(counter).toBe(1);
+  map.set('zero', 1);
+  expect(zero.get()).toBe(true);
+
+  map.delete('zero');
+  expect(zero.get()).toBe(false);
+  expect(counter).toBe(3);
+});
+
+test('obsArray find', () => {
+  const list = obsArray(null, [1, 2, 3]);
+  const one = list.find(x => x === 1);
+  expect(one.get()).toStrictEqual(1);
+  list.set(0, 2);
+  expect(one.get()).toStrictEqual(undefined);
 });
 
 test('obsSet', () => {
@@ -49,9 +105,8 @@ test('obsSet', () => {
   expect(hasA.get()).toBe(false);
 });
 
-
 test('obsJson', () => {
-  const json = obsJson(null, { foo: 1, bar: 2 });
+  const json = obsJson(null, {foo: 1, bar: 2});
   expect(json.get()).toStrictEqual({foo: 1, bar: 2});
   json.set('foo', 10);
   expect(json.get('foo')).toBe(10);
@@ -61,7 +116,10 @@ test('obsJson', () => {
   expect(json.keys()).toStrictEqual(Object.keys({foo: 10, bar: 2}));
 
   let counter = 0;
-  const foo = computed(null, () => { counter++; return json.get('foo'); });
+  const foo = computed(null, () => {
+    counter++;
+    return json.get('foo');
+  });
   expect(foo.get()).toBe(10);
   expect(counter).toBe(1);
   json.set('bar', 33);
@@ -71,7 +129,7 @@ test('obsJson', () => {
   expect(foo.get()).toBe(23);
   expect(counter).toBe(2);
   json.set({foo: 9, bar: 4});
-  
+
   expect(foo.get()).toBe(9);
   expect(counter).toBe(3);
 });
--- src/lib.ts
+++ src/lib.ts
@@ -1,17 +1,27 @@
-import { Computed, computed, ComputedImpl, observable, Observable, ObservableImpl, Owner, ComputedFunc } from './observables';
+import {
+  Computed,
+  computed,
+  ComputedImpl,
+  observable,
+  Observable,
+  ObservableImpl,
+  Owner,
+  ComputedFunc,
+  bundleChanges,
+} from './observables';
 
 /**
  * Computed observable that can have its own local state before refresh.
  */
-export function reactive<T>(owner: Owner|null, fun:ComputedFunc<T>): Computed<T> {
+export function reactive<T>(owner: Owner | null, fun: ComputedFunc<T>): Computed<T> {
   const comp = new ComputedImpl(owner, fun);
   comp.onWrite(val => comp.write(val));
   return comp;
 }
 
-export interface ObsArray<T = any> extends Observable<T[]> {
+export interface ObsArray<T = any> {
   get(): T[];
-  get(index: number): T|undefined;
+  get(index: number): T | undefined;
   set(value: T[]): void;
   set(index: number, value: T): void;
   push(...items: T[]): void;
@@ -19,63 +29,86 @@ export interface ObsArray<T = any> extends Observable<T[]> {
   splice(start: number, count: number, ...items: T[]): void;
   splice(start: number, count: number): void;
   splice(start: number): void;
+  map<K>(mapper: (value: T) => K): ObsArray<K>;
+  find(mapper: (value: T) => boolean): Observable<T | undefined>;
 }
 
-class ObservableArrayImpl<T>
-  extends ObservableImpl<T[]>
-  implements ObsArray<T>
-{
-  public _length: Observable<number>;
-  private elements: Observable<T>[] = [];
+class ObservableArrayImpl<T> extends Owner implements ObsArray<T> {
+  private _length: Observable<number>;
+  private _items: Observable<T>[] = [];
   constructor(owner: Owner | null, value: T[]) {
-    super(owner, value);
-    this._length = reactive(this, () => this.get().length);
+    super();
+    owner?.owns(this);
+    this._length = observable(this, value.length);
+    this._items = value.map(v => observable(this, v));
   }
   length(): number {
     return this._length.get();
   }
   push(...items: T[]) {
-    this.set(this.get().concat(...items));
+    bundleChanges(() => {
+      for (let i = 0; i <= items.length; i++) {
+        const myIndex = this.length() + i;
+        this._ensure(myIndex);
+        this.set(myIndex, items[i]);
+      }
+      this._length.set(this._length.get() + items.length);
+    });
+  }
+  _ensure(index: number) {
+    for (let i = this._length.peek(); i <= index; i++) {
+      this._items.push(observable(this, undefined as any));
+    }
   }
   splice(start: number, count?: number, ...items: T[]) {
-    const list = this.get().slice();
-    list.splice(start, count as any, ...items);
-    this.set(list);
+    const raw = this.get();
+    raw.splice(start, count ?? 0, ...items);
+    this.set(raw);
   }
-  get() : T[]
-  get(index: number): T | undefined
+  get(): T[];
+  get(index: number): T | undefined;
   get(index?: number): T | undefined | T[] {
     if (arguments.length === 0) {
-      return super.get();
+      return this._items.slice(0, this._length.get()).map(i => i.get());
     }
-    for(let i = this.elements.length; i <= index!; i++) {
-      this.elements.push(this.build(i));
-    }
-    return this.elements[index!].get();
+    this._ensure(index!);
+    return this._items[index!].get();
   }
-  set(value: T[]): void
-  set(index: number, value: T): void
+  set(value: T[]): void;
+  set(index: number, value: T): void;
   set(...args: any[]) {
-    if (arguments.length === 1) return super.set(args[0]);
-    this.splice(args[0], 1, args[1]);
+    if (arguments.length === 1) {
+      const value: T[] = args[0];
+      bundleChanges(() => {
+        for (let i = 0; i < Math.max(this._length.get(), value.length); i++) {
+          this._ensure(i);
+          this._items[i].set(value[i]);
+        }
+        this._length.set(value.length);
+      });
+    } else {
+      const index: number = args[0];
+      const value = args[1];
+      bundleChanges(() => {
+        this._ensure(index);
+        this._items[index].set(value);
+      });
+    }
   }
-  private build(index: number) {
-    return computed(
-      this,
-      () => {
-        return this.get()[index];
-      },
-      (val) => {
-        this.splice(index, 1, val);
-      }
-    );
+
+  map<K>(mapper: (value: T) => K): ObsArray<K> {
+    const raw = computed(this, () => this.get());
+    const other = obsArray(this, raw.get().map(mapper));
+    this.owns(raw.subscribe(val => other.set(val.map(mapper))));
+    return other;
+  }
+
+  find(mapper: (value: T) => boolean): Observable<T | undefined> {
+    return computed(this, () => this.get().find(mapper));
   }
 }
 
-export function obsArray<T = any>(
-  owner: Owner | null,
-  value: T[]
-): ObsArray<T> {
+export function obsArray<T = any>(owner: Owner | null, value: T[]): ObsArray<T> {
   return new ObservableArrayImpl(owner, value);
 }
 
@@ -88,27 +121,27 @@ export interface ObsSet<T = any> extends Observable<Set<T>> {
 }
 
 class ObsSetImpl<K> extends ObservableImpl<Set<K>> implements ObsSet<K> {
-  private keys = new Map<K, Computed<boolean>>();
+  private _keys = new Map<K, Computed<boolean>>();
   private _size = observable(this, 0);
-  constructor(owner: Owner|null, value: Set<K>) {
+  constructor(owner: Owner | null, value: Set<K>) {
     super(owner, value);
     this._size.set(value.size);
   }
   add(value: K): this {
     this.get().add(value);
     this.build(value);
-    this.keys.get(value)?.set(true);
+    this._keys.get(value)?.set(true);
     this._size.set(this.get().size);
     return this;
   }
   clear(): void {
     this.get().clear();
-    this.keys.forEach(val => val.set(false));
+    this._keys.forEach(val => val.set(false));
     this._size.set(0);
   }
   delete(value: K): boolean {
-    if (this.keys.has(value)) {
-      this.keys.get(value)?.set(false);
+    if (this._keys.has(value)) {
+      this._keys.get(value)?.set(false);
       this._size.set(this._size.get() - 1);
       return true;
     }
@@ -116,18 +149,21 @@ class ObsSetImpl<K> extends ObservableImpl<Set<K>> implements ObsSet<K> {
   }
   has(value: K): boolean {
     this.build(value);
-    return this.keys.get(value)!.get();
+    return this._keys.get(value)!.get();
   }
   size(): number {
     return this._size.get();
   }
   build(key: K) {
-    if (this.keys.has(key)) return;
-    this.keys.set(key, reactive(this, () => this.get().has(key)));
+    if (this._keys.has(key)) return;
+    this._keys.set(
+      key,
+      reactive(this, () => this.get().has(key))
+    );
   }
 }
 
-export function obsSet<T=any>(owner: Owner|null, value?: Set<T>): ObsSet<T> {
+export function obsSet<T = any>(owner: Owner | null, value?: Set<T>): ObsSet<T> {
   return new ObsSetImpl(owner, value ?? new Set());
 }
 
@@ -144,12 +180,15 @@ export interface ObsJson<T> extends Observable<T> {
 class ObsJsonImpl<T> extends ObservableImpl<T> implements ObsJson<T> {
   private _hasMap: Map<keyof T, Computed<boolean>> = new Map();
   private _getMap: Map<keyof T, Computed<any>> = new Map();
-  constructor(owner: Owner|null, value: T) {
+  constructor(owner: Owner | null, value: T) {
     super(owner, value);
   }
   has(key: keyof T): boolean {
     if (!this._hasMap.has(key)) {
-      this._hasMap.set(key, computed(this, () => key in this.get()));
+      this._hasMap.set(
+        key,
+        computed(this, () => key in this.get())
+      );
     }
     return this._hasMap.get(key)?.get() ?? false;
   }
@@ -160,12 +199,15 @@ class ObsJsonImpl<T> extends ObservableImpl<T> implements ObsJson<T> {
     return Object.values(this.get());
   }
   get<Z extends keyof T>(key: Z): T[Z];
-  get(): T
+  get(): T;
   get(...args: any[]): any {
     if (args.length === 0) return super.get();
     const key: keyof T = args[0];
     if (!this._getMap.has(key)) {
-      this._getMap.set(key, computed(this, () => this.get()[key]));
+      this._getMap.set(
+        key,
+        computed(this, () => this.get()[key])
+      );
     }
     return this._getMap.get(key)?.get();
   }
@@ -184,6 +226,78 @@ class ObsJsonImpl<T> extends ObservableImpl<T> implements ObsJson<T> {
   }
 }
 
-export function obsJson<T = any>(owner: Owner|null, value: T): ObsJson<T> {
+export function obsJson<T = any>(owner: Owner | null, value: T): ObsJson<T> {
   return new ObsJsonImpl(owner, value);
+}
+
+export interface ObsMap<K = string, V = any> extends Owner {
+  has(key: K): boolean;
+  get(): Map<K, V>;
+  get(key: K): V | undefined;
+  set(key: K, value: V): void;
+  keys(): K[];
+  values(): V[];
+  delete(key: K): void;
+}
+
+export function obsMap<K = string, V = any>(owner: Owner | null, value: any): ObsMap<K, V> {
+  return new ObsMapImpl(owner, value);
+}
+
+class ObsMapImpl extends Owner implements ObsMap<any, any> {
+  _map = new Map();
+  _obs: Map<any, Observable<any>> = new Map();
+  _version = observable(this, 0);
+  constructor(owner: Owner | null, value: any) {
+    super();
+    owner?.owns(this);
+    if (value instanceof Map) {
+      for (const key of value.keys()) {
+        this.set(key, value.get(key));
+      }
+    } else {
+      for (const key of Object.keys(value)) {
+        this.set(key, value[key]);
+      }
+    }
+  }
+  get(key: any): any;
+  get(): Map<any, any>;
+  get(key?: any): any {
+    if (key === undefined) {
+      return new Map([...this._map.entries()].map(e => [e[0], e[1]]));
+    }
+    if (!this._obs.has(key!)) {
+      this._obs.set(
+        key!,
+        computed(this, () => {
+          this._version.get();
+          return this._map.get(key!);
+        })
+      );
+    }
+    return this._obs.get(key!)?.get();
+  }
+  has(key: any): boolean {
+    return this.get(key) !== undefined;
+  }
+  set(key: any, value: any): void {
+    this._map.set(key, value);
+    this._inc();
+  }
+  _inc() {
+    this._version.set(this._version.get() + 1);
+  }
+  keys(): any[] {
+    this._version.get();
+    return Array.from(this._map.keys());
+  }
+  values(): any[] {
+    this._version.get();
+    return Array.from(this._map.values());
+  }
+  delete(key: any): void {
+    this._map.delete(key);
+    this._inc();
+  }
 }
